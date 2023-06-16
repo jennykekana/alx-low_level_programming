abfbea5568d4a5963b7f524357fc246f84257cf6
jennykekana <moketekekana@gmail.com>
2023-06-16 09:26:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	int s;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	s = n % 10;
+	if (s > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, s);
+	if (s == 0)
+		printf("Last digit of %d is %d and is 0\n", n, s)
+	if (s < 6 && s != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
+	return (0);
+}
