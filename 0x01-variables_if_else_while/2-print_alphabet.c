5a049d9e36f31194242d2f21955abf519fe55997
jennykekana <moketekekana@gmail.com>
2023-06-16 09:31:13 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,7 +17,7 @@ int main(void)
 	if (s > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, s);
 	if (s == 0)
-		printf("Last digit of %d is %d and is 0\n", n, s)
+		printf("Last digit of %d is %d and is 0\n", n, s);
 	if (s < 6 && s != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 	return (0);
