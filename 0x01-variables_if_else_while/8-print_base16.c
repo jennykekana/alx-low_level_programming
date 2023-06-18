a50cba0395b6e0e4dbec6784dce0a150d5a54734
jennykekana <moketekekana@gmail.com>
2023-06-18 07:55:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int n;
+	char low;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	for (low = 'b'; low <= 'm'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
